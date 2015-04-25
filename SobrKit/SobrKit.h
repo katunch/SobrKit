9c44a636e6e84cf8c79988cb39a70a19757c9b48
Silas Knobel <silas.knobel@katun.ch>
2015-04-25 13:09:11 +0200

    basic workspace

---
+++ SobrKit/SobrKit.h
@@ -0,0 +1,19 @@
+//
+//  SobrKit.h
+//  SobrKit
+//
+//  Created by Silas Knobel on 25/04/15.
+//  Copyright (c) 2015 katun.ch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SobrKit.
+FOUNDATION_EXPORT double SobrKitVersionNumber;
+
+//! Project version string for SobrKit.
+FOUNDATION_EXPORT const unsigned char SobrKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SobrKit/PublicHeader.h>
+
+
