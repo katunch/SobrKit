46c333745da9609d475db3ad38b4630d4e64e1c1
Silas Knobel <silas.knobel@katun.ch>
2015-05-15 10:48:51 +0200

    added nsdate extension

--- SobrKit/FoundationExtension.swift
+++ SobrKit/FoundationExtension.swift
@@ -190,19 +190,91 @@ public extension NSURL {
     :returns: `[String: String]` Array of all query parameters.
     */
     func queryParameters() -> [String: String] {
-        
-        let query = self.query!
-        let keyValuePairs = split(query) { $0 == "&" }
-        
         var returnData: [String: String] = [String: String]()
         
-        for keyValuePair in keyValuePairs {
-            let splits = split(keyValuePair) {$0 == "="}
-            let key = splits[0] as String
-            let value = splits[1] as String
-            returnData[key] = value
+        if let query = self.query {
+            let keyValuePairs = split(query) { $0 == "&" }
+            
+            for keyValuePair in keyValuePairs {
+                let splits = split(keyValuePair) {$0 == "="}
+                let key = splits[0] as String
+                let value = splits[1] as String
+                returnData[key] = value
+            }
         }
         
         return returnData
+    }
+}
+
+//MARK: - NSDate
+extension NSDate
+{
+    func isGreaterThanDate(dateToCompare : NSDate) -> Bool
+    {
+        //Declare Variables
+        var isGreater = false
+        
+        //Compare Values
+        if self.compare(dateToCompare) == NSComparisonResult.OrderedDescending
+        {
+            isGreater = true
+        }
+        
+        //Return Result
+        return isGreater
+    }
+    
+    
+    func isLessThanDate(dateToCompare : NSDate) -> Bool
+    {
+        //Declare Variables
+        var isLess = false
+        
+        //Compare Values
+        if self.compare(dateToCompare) == NSComparisonResult.OrderedAscending
+        {
+            isLess = true
+        }
+        
+        //Return Result
+        return isLess
+    }
+    
+    
+    func isEqualToDate(dateToCompare : NSDate) -> Bool
+    {
+        //Declare Variables
+        var isEqualTo = false
+        
+        //Compare Values
+        if self.compare(dateToCompare) == NSComparisonResult.OrderedSame
+        {
+            isEqualTo = true
+        }
+        
+        //Return Result
+        return isEqualTo
+    }
+    
+    
+    
+    func addDays(daysToAdd : Int) -> NSDate
+    {
+        var secondsInDays : NSTimeInterval = Double(daysToAdd) * 60 * 60 * 24
+        var dateWithDaysAdded : NSDate = self.dateByAddingTimeInterval(secondsInDays)
+        
+        //Return Result
+        return dateWithDaysAdded
+    }
+    
+    
+    func addHours(hoursToAdd : Int) -> NSDate
+    {
+        var secondsInHours : NSTimeInterval = Double(hoursToAdd) * 60 * 60
+        var dateWithHoursAdded : NSDate = self.dateByAddingTimeInterval(secondsInHours)
+        
+        //Return Result
+        return dateWithHoursAdded
     }
 }
